c8208d9fe8a77881f666585aa661e4bd7dd606fe
Tabalin Nikolay <Zerru_Kun@yahoo.com>
2022-11-01 15:14:01 +0300

    Tasks counter.

--- src/store/tasks.js
+++ src/store/tasks.js
@@ -43,20 +43,11 @@ class Tasks {
   chooseCard(el, addTo, deleteFrom) {
     addTo.push(el);
     for (var i = 0; i < deleteFrom.length; i++) {
-    if (deleteFrom[i].id === el.id) {
-      deleteFrom.splice(i, 1);
-      i--;
-    };
-
-  }
-    // deleteFrom = deleteFrom.map(task => task.id === el.id ? delete deleteFrom[task.id] : task);
-    //delete deleteFrom[task]
-    // console.log(addTo);
-    // console.log(deleteFrom);
-    // console.log("id  " + el.id)
-    // deleteFrom.map(el => {
-    //   return console.log("id delete  " + el.id)
-    // })
+      if (deleteFrom[i].id === el.id) {
+        deleteFrom.splice(i, 1);
+        i--;
+      };
+    }
   }
 }
 
