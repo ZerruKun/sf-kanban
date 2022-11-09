964cde2c4631321c8e58a81e0e6eaa2860f0dbef
Tabalin Nikolay <Zerru_Kun@yahoo.com>
2022-11-09 22:21:51 +0300

    Transfer receiving localStorage date to App.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,35 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
+import tasks from "./store/tasks";
+import { observer } from "mobx-react-lite";
 
-const App = () => {
+const App = observer(() => {
+
+  //Получение данных из localStorage и распределине по категориям
+  const getLocalStorageCards = () => {
+    let storage = [];
+    let keys = Object.keys(localStorage);
+    let i = keys.length;
+    while (i--) {
+      storage[keys[i]] = JSON.parse(localStorage.getItem(keys[i]));
+    }
+    //При таком подходе первый объект всегда получается пустым - убираем его
+    storage.shift();
+    for (let i = 0; i < storage.length; i++) {
+      if(storage[i].category === "backlog") {
+        tasks.backlog.push(storage[i]);
+      } else if(storage[i].category === "ready") {
+        tasks.ready.push(storage[i]);
+      } else if(storage[i].category === "inProgress") {
+        tasks.inProgress.push(storage[i]);
+      } else if(storage[i].category === "finished") {
+        tasks.finished.push(storage[i]);
+      }        
+    }
+  }
+  
+  getLocalStorageCards();
 
   return (
     <div className="App">
@@ -15,6 +42,6 @@ const App = () => {
       <Footer />
     </div>
   );
-};
+});
 
 export default App;
--- src/components/Lists/Lists.jsx
+++ src/components/Lists/Lists.jsx
@@ -2,35 +2,8 @@ import React from "react";
 import styles from "./Lists.module.css";
 import List from "../List/List";
 import tasks from "../../store/tasks"
-import { observer } from "mobx-react-lite";
 
-const Lists = observer(() => {
-  
-  //Получение данных из localStorage и распределине по категориям
-  const getLocalStorageCards = () => {
-    let storage = [];
-    let keys = Object.keys(localStorage);
-    let i = keys.length;
-    while (i--) {
-      storage[keys[i]] = JSON.parse(localStorage.getItem(keys[i]));
-    }
-    //При таком подходе первый объект всегда получается пустым - убираем его
-    storage.shift();
-    for (let i = 0; i < storage.length; i++) {
-      if(storage[i].category === "backlog") {
-        tasks.backlog.push(storage[i]);
-      } else if(storage[i].category === "ready") {
-        tasks.ready.push(storage[i]);
-      } else if(storage[i].category === "inProgress") {
-        tasks.inProgress.push(storage[i]);
-      } else if(storage[i].category === "finished") {
-        tasks.finished.push(storage[i]);
-      }        
-    }
-  }
-
-  getLocalStorageCards();
-
+const Lists = () => {
   //Title задан строкой, так как не планируется к изменению
   return (
     <div className={styles.general}>
@@ -40,6 +13,6 @@ const Lists = observer(() => {
       <List tasks={tasks.finished} choice={tasks.inProgress} title="Finished"/>
     </div>
   );
-});
+};
 
 export default Lists;
